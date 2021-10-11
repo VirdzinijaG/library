53d6e5356befc9808c0bff4a3eaa2cb2e0866288
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-11 18:12:13 +0300

    10.11

--- biblioteka/src/Book.jsx
+++ biblioteka/src/Book.jsx
@@ -1,12 +1,32 @@
-function Book({ book }) {
+function Book({ book, deleteBook, showModal }) {
   return (
-    <div className="col-4">
-      <div className="card">
+    <div className="col-lg-4 col-md-6">
+      <div className="card m-3">
         <div className="card-body">
           <h5 className="card-title">{book.title}</h5>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <h6>{book.author}</h6>
+          <span className="badge badge-pill badge-secondary m-1 p-2">
+            category: {book.category}
+          </span>
+          <span className="badge badge-pill badge-secondary m-1 p-2">
+            pages: {book.pages}
+          </span>
+          <div className="form-group mt-3">
+            <button
+              type="button"
+              className="btn btn-danger m-1"
+              onClick={() => deleteBook(book.id)}
+            >
+              Delete
+            </button>
+            <button
+              type="button"
+              className="btn btn-success m-1"
+              onClick={() => showModal(book.id)}
+            >
+              Edit
+            </button>
+          </div>
         </div>
       </div>
     </div>
