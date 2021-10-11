ad941c7349f80cb461d556e2917e33327d72b9e5
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-11 18:11:42 +0300

    10.11

--- biblioteka/src/Book.jsx
+++ biblioteka/src/Book.jsx
@@ -1,8 +1,16 @@
 function Book({ book }) {
-  return <div className="col-4">
-      book
-      </div>;
+  return (
+    <div className="col-4">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{book.title}</h5>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 
 export default Book;
--- biblioteka/src/Books.jsx
+++ biblioteka/src/Books.jsx
@@ -1,24 +1,22 @@
 import { useEffect, useState } from "react";
 import Book from "./Book";
+import axios from "axios";
 
 function Books() {
-  function Books() {
-    const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-      axios.get("http;//localhost:3003/books").then((response) => {
-        setBooks(response.data);
-      });
-    }, []);
-  }
+  useEffect(() => {
+    axios.get("http://localhost:3003/books")
+    .then((response) => {
+      setBooks(response.data);
+    });
+  }, []);
 
   return (
     <>
       <div className="container">
         <div className="row">
-          {books.map((book) => (
-            <Book key={book.id} book={book}></Book>
-          ))}
+          {books.map(book => <Book key={book.id} book={book}></Book> )}
         </div>
       </div>
     </>
