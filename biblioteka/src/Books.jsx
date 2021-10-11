5e49f2904f4840c9771549492bf235d5d9740d33
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-11 18:12:42 +0300

    10.11

--- biblioteka/src/Books.jsx
+++ biblioteka/src/Books.jsx
@@ -1,22 +1,18 @@
-import { useEffect, useState } from "react";
 import Book from "./Book";
-import axios from "axios";
 
-function Books() {
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3003/books")
-    .then((response) => {
-      setBooks(response.data);
-    });
-  }, []);
-
+function Books({ books, deleteBook, showModal }) {
   return (
     <>
       <div className="container">
         <div className="row">
-          {books.map(book => <Book key={book.id} book={book}></Book> )}
+          {books.map((book) => (
+            <Book
+              key={book.id}
+              book={book}
+              deleteBook={deleteBook}
+              showModal={showModal}
+            />
+          ))}
         </div>
       </div>
     </>
